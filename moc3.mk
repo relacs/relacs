ce84c4f4daa30d680e5da34a54ed64261605f573
Sebastian Pipping <hartwork>
2008-03-28 19:37:21 +0000

    Move <hardware/*.cc> to <hardware/src/*.cc> part 3
    git-svn-id: https://svn.code.sf.net/p/relacs/code/relacs/trunk@110 1550813e-0d35-0410-aec7-fc86b090456a

--- moc3.mk
+++ moc3.mk
@@ -1,12 +1,5 @@
-MOC_CREATE = $(MOC) -i -o "$@" "$<"
-
-hardware/moc_%.cc : hardware/include/relacs/%.h
-	$(MOC_CREATE)
-
 moc_%.cc : ../include/relacs/%.h
-	$(MOC_CREATE)
-
-
+	$(MOC) -i -o "$@" "$<"
 
 clean-moc-extra:
 	rm -vf moc_*.cc
