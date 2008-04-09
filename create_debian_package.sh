89c6a3923deac36dde8dd94e27a7f234fffda1c5
Sebastian Pipping <hartwork>
2008-04-09 04:10:05 +0000

    More Debian bits
    git-svn-id: https://svn.code.sf.net/p/relacs/code/relacs/trunk@143 1550813e-0d35-0410-aec7-fc86b090456a

--- create_debian_package.sh
+++ create_debian_package.sh
@@ -1,22 +1,21 @@
 #! /bin/sh
 VERSION=0.9.6
 
-PWD_BACKUP=$PWD
-
 PKGDIR=relacs-${VERSION}
 PKGFILE=${PKGDIR}.tar.gz
 ORIGFILE=../relacs_${VERSION}.orig.tar.gz
+CHANGESFILE=../relacs_${VERSION}.changes
 
 if [ ! -f ${PKGFILE} ]; then
     ./bootstrap.sh || exit 1
     make dist || exit 1
 fi
 
-if [ ! -f ${ORIGFILE} ]; then
-    cp ${PKGFILE} ${ORIGFILE} || exit 1
-fi
+cp ${PKGFILE} ${ORIGFILE} || exit 1
 
 dpkg-buildpackage -us -uc -rfakeroot -nc || exit 1
+lintian "${CHANGESFILE}" -i || exit 1
 
+echo DONE.
 exit 0
 
