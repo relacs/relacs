e06b6947aed3bcdfaf660eeee7cb9e70ce6358f7
Jan Benda <jan.benda@uni-tuebingen.de>
2009-05-28 16:26:53 +0000

    Added xmoments.cc example for testing mean, variance, skewness, and kurtosis
    git-svn-id: https://svn.code.sf.net/p/relacs/code/relacs/trunk@398 1550813e-0d35-0410-aec7-fc86b090456a

---
+++ numerics/examples/xmoments.cc
@@ -0,0 +1,79 @@
+/*
+  xmoments.cc
+  check mean, stdev, skewness, and kurtosis for Gaussian and Exponential distributions.
+
+  RELACS - Relaxed ELectrophysiological data Acquisition, Control, and Stimulation
+  Copyright (C) 2002-2009 Jan Benda <j.benda@biologie.hu-berlin.de>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  RELACS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cstdio>
+#include <vector>
+#include <relacs/stats.h>
+#include <relacs/array.h>
+using namespace relacs;
+
+
+void moments( const ArrayD &x )
+{
+  /*
+  double a = mean( x );
+  double v = variance( x );
+  double s = stdev( x );
+  double sk = skewness( x );
+  double kt = kurtosis( x );
+  */
+
+  double a = x.mean();
+  double v = x.variance();
+  double s = x.stdev();
+  double sk = x.skewness();
+  double kt = x.kurtosis();
+
+  cout << "mean: " << a << '\n';
+  cout << "variance: " << v << '\n';
+  cout << "standard deviation: " << s << '\n';
+  cout << "skewness: " << sk << '\n';
+  cout << "kurtosis: " << kt << '\n';
+}
+
+
+int main( int argc, char **argv )
+{
+  ArrayD x;
+  const int n = 1000000;
+
+  cout << "\nUniform distribution (s=0, k=-6/5):\n";
+  x.rand( n, rnd );
+  moments( x );
+
+  cout << "\nNormal distribution (s=0, k=0):\n";
+  for ( ArrayD::iterator p = x.begin(); p != x.end(); ++p )
+    *p = rnd.gaussian();
+  //  x.randNorm( n, rnd );
+  moments( x );
+
+  cout << "\nExponential distribution (s=2, k=6):\n";
+  for ( ArrayD::iterator p = x.begin(); p != x.end(); ++p )
+    *p = rnd.exponential();
+  moments( x );
+
+  cout << "\nGamma distribution a=4 (s=1, k=3/2):\n";
+  for ( ArrayD::iterator p = x.begin(); p != x.end(); ++p )
+    *p = rnd.gamma( 4.0 );
+  moments( x );
+
+  return 0;
+}
