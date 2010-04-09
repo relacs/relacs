c0f276da298cfbf79fa7124af491fcb6e265e85d
Jan Benda <jan.benda@uni-tuebingen.de>
2010-04-09 15:39:39 +0000

    Testing analog trigger functionality of NI M Series boards
    git-svn-id: https://svn.code.sf.net/p/relacs/code/relacs/trunk@585 1550813e-0d35-0410-aec7-fc86b090456a

--- plugins/linuxdevices/comedi/examples/ao_trigger.c
+++ plugins/linuxdevices/comedi/examples/ao_trigger.c
@@ -22,15 +22,18 @@ int main(int argc, char *argv[])
   comedi_range *rng;
   int ret;
   struct parsed_options options;
-  int fn;
+  int fn = 3;
   sampl_t *data;
+  comedi_insn aotrigger;
+  sampl_t tdata[5];
+
 
   init_parsed_options(&options);
   options.subdevice = -1;
   options.n_chan = 100000;/* default number of samples */
   parse_options(&options, argc, argv);
 
-  /* Use n_chan to set buffer size */
+  /* Use n_chan to set number of data points */
   fn = options.n_chan;
   if(fn <= 0){
     fprintf(stderr, "invalid buffer size %d\n", fn );
@@ -47,23 +50,6 @@ int main(int argc, char *argv[])
   }
   if(options.subdevice < 0)
     options.subdevice = comedi_find_subdevice_by_type(dev, COMEDI_SUBD_AO, 0);
-
-  /*! This is important to make write calls non blocking! */
-  fcntl(comedi_fileno(dev), F_SETFL, O_NONBLOCK);
-
-  maxbuffersize = comedi_get_max_buffer_size( dev, options.subdevice );
-  printf( "maximum buffer_size: %d\n", maxbuffersize );
-
-  /* you must be root!
-  ret = comedi_set_max_buffer_size( dev, options.subdevice, 10*maxbuffersize );
-  if ( ret < 0) {
-    comedi_perror("comedi_set_max_buffer_size");
-    exit(1);
-  }
-  */
-
-  comedi_set_buffer_size( dev, options.subdevice, maxbuffersize );
-  printf( "buffer_size: %d\n", comedi_get_buffer_size( dev, options.subdevice ) );
 
   maxdata = comedi_get_maxdata(dev, options.subdevice, options.channel);
   rng = comedi_get_range(dev, options.subdevice, options.channel, options.range);
@@ -87,14 +73,16 @@ int main(int argc, char *argv[])
 
   chanlist[0] = CR_PACK(options.channel, options.range, options.aref);
 
-  /* init data buffer (a ramp with a final 0): */
+  /* init data buffer (a pulse with a final 0): */
   data = (sampl_t *)malloc( fn*sizeof( sampl_t ) );
   if(data == NULL ){
     perror("malloc\n");
     exit(1);
   }
-  for ( n=0; n<fn; n++ )
-    data[n] = (sampl_t)( maxdata*(double)n/fn );
+  for ( n=0; n<fn-1; n++ )
+    //    data[n] = (sampl_t)( maxdata*(double)n/fn );
+    data[n] = maxdata-1;
+    //  data[n] = comedi_from_phys((double)n/fn, rng, maxdata);
   data[fn-1] = comedi_from_phys(0.0, rng, maxdata);
 
   dump_cmd(stdout,&cmd);
@@ -121,6 +109,30 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
+  /* Setup analog trigger: */
+  memset(&aotrigger,0,sizeof(aotrigger));
+  aotrigger.insn = INSN_CONFIG_ANALOG_TRIG;
+  aotrigger.n = 5;
+  aotrigger.data = &tdata;
+  aotrigger.subdev = 0;
+  aotrigger.chanspec = 0;
+  tdata[0] = 0;
+  tdata[1] = 1;
+  tdata[2] = CR_PACK(6, 0, AREF_GROUND);
+  tdata[3] = comedi_from_phys(0.0, rng, maxdata);
+  tdata[4] = 0;
+  ret = comedi_do_insn( dev, &aotrigger );  
+  if(ret < 0){
+    comedi_perror("comedi_do_insn");
+    perror("comedi_do_insn");
+    exit(1);
+  }
+
+  /* Route AO_START1 to PFI0: */
+  //  comedi_set_routing( dev, 7, 0, NI_PFI_OUTPUT_AO_START1 );
+  comedi_set_routing( dev, 7, 0, NI_PFI_OUTPUT_I_ATRIG );
+  comedi_dio_config( dev, 7, 0, COMEDI_OUTPUT );
+
   n = fn*sizeof( sampl_t );
   printf( "preload analog output buffer with %d bytes...\n", n );  
   m = write(comedi_fileno(dev), (void *)data, n );
@@ -131,23 +143,6 @@ int main(int argc, char *argv[])
   printf( "... took %d bytes\n", m );
   n -= m;  
 
-  printf( "buffer_contents: %d\n", comedi_get_buffer_contents( dev, cmd.subdev ) );
-
-  /* A second call to write blocks for ever! If device is opened in blocking mode (default) */
-  /*
-  printf( "preload analog output buffer with %d bytes...\n", n );  
-  m = write(comedi_fileno(dev), (void *)data+(fn*sizeof( sampl_t )-n), n );
-  if(m < 0){
-    if ( errno != EAGAIN ) {
-      perror("preload write");
-      exit(1);
-    }
-    else
-      fprintf( stderr, "... no more data can be filled! Try later.\n" );
-  }
-  printf( "... took %d bytes\n", m );
-  n -= m;  
-  */
   ret = comedi_internal_trigger(dev, options.subdevice, 0);
   if(ret < 0){
     perror("comedi_internal_trigger\n");
@@ -155,24 +150,17 @@ int main(int argc, char *argv[])
   }
   
   while( n > 0 ) {
-    printf( "buffer_contents: %d\n", comedi_get_buffer_contents( dev, cmd.subdev ) );
     m = write(comedi_fileno(dev),(void *)data+(fn*sizeof( sampl_t )-n),n);
     if(m<0){
-      if ( errno != EAGAIN ) {
-	perror("write");
-	exit(0);
-      }
-      else {
-	fprintf( stderr, "... no more data can be filled! Try later.\n" );
-	usleep( 100000 );
-      }
+      perror("write");
+      exit(0);
     }
     else {
       printf("m=%d\n",m);
       n-=m;
     }
   }
-  
+
   while ( comedi_get_subdevice_flags( dev, cmd.subdev ) & SDF_RUNNING )
     usleep( 100000 );
 
