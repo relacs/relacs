64720aa72bfa46b0e1fc54082c6ec9ec5be3a22e
Jan Benda <jan.benda@uni-tuebingen.de>
2011-10-07 09:19:43 +0000

    - Updated dynclam documentation - disabled TTL pulse generation by default - new model with voltage gated current, but needs exp() function to be enabled...
    git-svn-id: https://svn.code.sf.net/p/relacs/code/relacs/trunk@787 1550813e-0d35-0410-aec7-fc86b090456a

---
+++ plugins/linuxdevices/comedi/module/leakvgatevcmodel.c
@@ -0,0 +1,67 @@
+
+  /*! Name, by which this module is known inside Linux: */
+char *moduleName;
+
+  /*! The period length of the realtime periodic task in seconds. */
+float loopInterval;
+  /*! One over the period length of the realtime periodic task in Hertz. */
+float loopRate;
+
+  /*! Analog input that is read from the DAQ board. */
+#define INPUT_N 1
+  /*! The \a inputNames are used to match the \a input variables with
+      analog input traces in Relacs. */
+char *inputNames[INPUT_N] = { "V-1" };
+char *inputUnits[INPUT_N] = { "mV" };
+  /*! The \a inputChannels and \a inputDevices are set automatically. */
+int inputChannels[INPUT_N];
+int inputDevices[INPUT_N];
+  /*! \a input holds the current value that was read in from the DAQ board. */
+float input[INPUT_N] = { 0.0 };
+
+  /*! Analog output that is written to the DAQ board. */
+#define OUTPUT_N 1
+char *outputNames[OUTPUT_N] = { "Current-1" };
+char *outputUnits[OUTPUT_N] = { "nA" };
+int outputChannels[OUTPUT_N];
+int outputDevices[OUTPUT_N];
+float output[OUTPUT_N] = { 0.0 };
+
+  /*! Parameter that are provided by the model and can be read out. */
+#define PARAMINPUT_N 3
+char *paramInputNames[PARAMINPUT_N] = { "Leak-current", "VC-current", "Voltage-gated current" };
+char *paramInputUnits[PARAMINPUT_N] = { "nA", "nA" };
+float paramInput[PARAMINPUT_N] = { 0.0, 0.0, 0.0 };
+
+  /*! Parameter that are read by the model and are written to the model. */
+#define PARAMOUTPUT_N 9
+char *paramOutputNames[PARAMOUTPUT_N] = { "g", "E", "VCgain", "VC", "gvgate", "Evgate", "vgatetau", "vgatevmid", "vgatevwidth" };
+char *paramOutputUnits[PARAMOUTPUT_N] = { "nS", "mV", "mS", "mV", "nS", "mV", "ms", "mV", "mV" };
+float paramOutput[PARAMOUTPUT_N] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 10.0, 0.0, 10.0 };
+
+  /*! Variables used by the model. */
+float meaninput = 0.0;
+float vgate = 0.0;
+
+void initModel( void )
+{
+   moduleName = "/dev/dynclamp";
+   meaninput = 0.0;
+   vgate = 0.0;
+}
+
+void computeModel( void )
+{
+  // running average:
+  meaninput += (input[0] - meaninput)/5.0;  // steps
+
+  // leak:
+  paramInput[0] = -0.001*paramOutput[0]*(input[0]-paramOutput[1]);
+  // voltage clamp:
+  paramInput[1] = -paramOutput[2]*(meaninput-paramOutput[3]);
+  // voltage gated channel:
+  vgate += loopInterval*1000.0/paramOutput[6]*(-vgate+1.0/(1.0+exp(-(input[0]-paramOutput[7])/paramOutput[8])));
+  paramInput[2] = -0.001*paramOutput[4]*vgate*(input[0]-paramOutput[5]);
+  // total injected current:
+  output[0] = paramInput[0] + paramInput[1] + paramInput[2];
+}
--- plugins/linuxdevices/comedi/module/loadmodules.sh
+++ plugins/linuxdevices/comedi/module/loadmodules.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+# This script loads all the necessary kernal modules that are needed
+# for using dynamic clamp.
+# This requires an RTAI patched linux kernel.
+# See www.relacs.net/plugins/comedi/index.html for more information.
+# If you want to load the kernel modules automatically by the boot
+# process of your linux system, simply call this script from /etc/rc.local
+
 echo "please wait..."
 modprobe rtai_fifos
 modprobe kcomedilib
 
 test -c /dev/dynclamp || mknod -m 666 /dev/dynclamp c 227 0
 
-#insmod rtmodule.ko && echo "loaded rtmodule.ko"
+MODULEPATH="${0%/*}"
 
-insmod dynclampmodule.ko && echo "loaded dynclampmodule.ko"
+#insmod $MODULEPATH/rtmodule.ko && echo "loaded $MODULEPATH/rtmodule.ko"
+
+insmod $MODULEPATH/dynclampmodule.ko && echo "loaded $MODULEPATH/dynclampmodule.ko"
