594bfb38e76888fe0b946b80aaf3e59f0efc4edc
Jan Benda <jan.benda@uni-tuebingen.de>
2014-02-18 20:35:03 +0000

    New InputRangeCheck RePro
    git-svn-id: https://svn.code.sf.net/p/relacs/code/relacs/trunk@1078 1550813e-0d35-0410-aec7-fc86b090456a

---
+++ plugins/calibration/include/relacs/calibration/inputrangecheck.h
@@ -0,0 +1,66 @@
+/*
+  calibration/inputrangecheck.h
+  Checks calibration of each range (gain) of an analag input channel.
+
+  RELACS - Relaxed ELectrophysiological data Acquisition, Control, and Stimulation
+  Copyright (C) 2002-2012 Jan Benda <benda@bio.lmu.de>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  RELACS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _RELACS_CALIBRATION_INPUTRANGECHECK_H_
+#define _RELACS_CALIBRATION_INPUTRANGECHECK_H_ 1
+
+#include <relacs/plot.h>
+#include <relacs/repro.h>
+using namespace relacs;
+
+namespace calibration {
+
+
+/*!
+\class InputRangeCheck
+\brief [RePro] Checks calibration of each range (gain) of an analag input channel.
+\author Jan Benda
+\version 1.0 (Feb 18, 2014)
+*/
+
+
+class InputRangeCheck : public RePro
+{
+  Q_OBJECT
+
+public:
+
+  InputRangeCheck( void );
+  virtual void preConfig( void );
+  virtual void notify( void );
+  virtual int main( void );
+
+
+protected:
+ 
+  Plot P;
+
+  string InName;
+  string InUnit;
+  string OutName;
+  string OutUnit;
+
+};
+
+
+}; /* namespace calibration */
+
+#endif /* ! _RELACS_CALIBRATION_INPUTRANGECHECK_H_ */
---
+++ plugins/calibration/src/inputrangecheck.cc
@@ -0,0 +1,185 @@
+/*
+  calibration/inputrangecheck.cc
+  Checks calibration of each range (gain) of an analag input channel.
+
+  RELACS - Relaxed ELectrophysiological data Acquisition, Control, and Stimulation
+  Copyright (C) 2002-2012 Jan Benda <benda@bio.lmu.de>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  RELACS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <relacs/rangeloop.h>
+#include <relacs/tablekey.h>
+#include <relacs/calibration/inputrangecheck.h>
+using namespace relacs;
+
+namespace calibration {
+
+
+InputRangeCheck::InputRangeCheck( void )
+  : RePro( "InputRangeCheck", "calibration", "Jan Benda", "1.0", "Feb 18, 2014" )
+{
+  // add some options:
+  addSelection( "outtrace", "Output trace", "V-1" );
+  addSelection( "intrace", "Input trace", "V-1" );
+  addNumber( "duration", "Stimulus duration", 1.0, 0.001, 100000.0, 0.001, "s", "s" );
+  addInteger( "amplnum", "Number of amplitudes to be tested", 10, 0, 100000 );
+
+  setWidget( &P );
+}
+
+
+void InputRangeCheck::preConfig( void )
+{
+  setText( "intrace", traceNames() );
+  setToDefault( "intrace" );
+  setText( "outtrace", outTraceNames() );
+  setToDefault( "outtrace" );
+}
+
+
+void InputRangeCheck::notify( void )
+{
+  int outtrace = index( "outtrace" );
+  if ( outtrace >= 0 && outtrace < outTracesSize() ) {
+    OutName = outTrace( outtrace ).traceName();
+    OutUnit = outTrace( outtrace ).unit();
+  }
+
+  int intrace = index( "intrace" );
+  if ( intrace >= 0 && intrace < traces().size() ) {
+    InName = trace( intrace ).ident();
+    InUnit = trace( intrace ).unit();
+  }
+}
+
+
+int InputRangeCheck::main( void )
+{
+  // get options:
+  int outtrace = index( "outtrace" );
+  int intrace = index( "intrace" );
+  double duration = number( "duration" );
+  int amplnum = integer( "amplnum" );
+
+  // don't print repro message:
+  noMessage();
+
+  // input gain setting:
+  int orggain = trace( intrace ).gainIndex();
+
+  // plot trace:
+  tracePlotSignal( duration, 0.0 );
+
+  // init plot:
+  P.lock();
+  P.setXLabel( "Analog output " + OutName + " [" + OutUnit + "]" );
+  P.setYLabel( "Analog input " + InName + " [" + InUnit + "]" );
+  P.unlock();
+
+  // ranges:
+  vector<double> ranges;
+  maxValues( trace( intrace ), ranges );
+  for ( unsigned int r=0; r < ranges.size() && softStop() < 1; ++r ) {
+
+    // init plot:
+    P.lock();
+    P.setTitle( "Input range " + Str( r ) + ": " +
+		Str( -ranges[r] ) + " --- " + Str( ranges[r] ) + " " + InUnit );
+    P.setXRange( -1.05*ranges[r], 1.05*ranges[r] );
+    P.setYRange( -1.05*ranges[r], 1.05*ranges[r] );
+    P.unlock();
+
+    setGain( trace( intrace ), r );
+
+    RangeLoop amplrange( -ranges[r], ranges[r], amplnum, 1, 1, 1 );
+    MapD ampls;
+    ampls.reserve( amplrange.size() );
+  
+    for ( amplrange.reset(); ! amplrange && softStop() < 2; ++amplrange ) {
+
+      double amplitude = *amplrange;
+
+      //  ouptut signal:
+      OutData signal;
+      signal.setTrace( outtrace );
+      signal.pulseWave( duration, -1.0, amplitude, 0.0 );
+      write( signal );
+      if ( signal.failed() )
+	return Failed;
+      
+      message( "Input range <b>" + Str( r ) + "</b>: Test amplitude <b>" + 
+	       Str( amplitude ) + " " + InUnit + "</b>" );
+
+      sleep( signal.length() );
+      if ( interrupt() )
+	return Aborted;
+
+      // analyze:
+      double d = signal.length();
+      double val = trace( intrace ).mean( signalTime() + 0.05*d, signalTime()+0.95*d );
+      ampls.push( amplitude, val );
+
+      P.lock();
+      P.clear();
+      P.plotVLine( -ranges[r], Plot::White, 3 );
+      P.plotVLine( ranges[r], Plot::White, 3 );
+      P.plotVLine( 0.0, Plot::White, 3 );
+      P.plotHLine( 0.0, Plot::White, 3 );
+      P.plotLine( -ranges[r], -ranges[r], ranges[r], ranges[r], Plot::Yellow, 2 );
+      P.plot( ampls, 1.0, Plot::Red, 2, Plot::Solid,
+	      Plot::Circle, 10, Plot::Red, Plot::Red );
+      P.draw();
+      P.unlock();
+      
+      sleep( 0.1 );
+    }
+
+    // save data:
+    ofstream df( addPath( "inputrangecheck-data.dat" ).c_str(),
+		 ofstream::out | ofstream::app );
+    if ( r == 0 ) {
+      df << '\n';
+      settings().save( df, "# ", 0, Options::FirstOnly );
+      df << '\n';
+    }
+    df << "# gainindex: " << Str( r ) << '\n';
+    df << "# range: " << Str( ranges[r] ) << InUnit << '\n';
+    df << '\n';
+    TableKey datakey;
+    datakey.addNumber( InName, InUnit, "%8.3f" );
+    datakey.addNumber( OutName, OutUnit, "%8.3f" );
+    datakey.addNumber( "diff", OutUnit, "%8.4f" );
+    datakey.saveKey( df );
+    double infac = Parameter::changeUnit( 1.0, InUnit, OutUnit );
+    for ( int k=0; k<ampls.size(); k++ ) {
+      datakey.save( df, ampls.x( k ), 0 );
+      datakey.save( df, ampls.y( k ) );
+      datakey.save( df, ampls.y( k ) - infac * ampls.x( k ) );
+      df << '\n';
+    }
+    df << "\n";
+
+  }
+
+  setGain( trace( intrace ), orggain );
+  return Completed;
+}
+
+
+addRePro( InputRangeCheck, calibration );
+
+}; /* namespace calibration */
+
+#include "moc_inputrangecheck.cc"
