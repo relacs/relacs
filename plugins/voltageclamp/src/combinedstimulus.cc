4c73eb091a331a6bc82c5f679a566eea059f2aeb
sonnenberg <Lukas.Sonnenberg@web.de>
2020-08-14 14:59:48 +0200

    safety

--- plugins/voltageclamp/src/combinedstimulus.cc
+++ plugins/voltageclamp/src/combinedstimulus.cc
@@ -82,6 +82,8 @@ int CombinedStimulus::main( void )
   bool samplingpulses = boolean( "samplingpulses" );
   double noiseduration = number( "noiseduration" );
 
+  double maxDurationForIVPlot = 50;
+
   // don't print repro message:
   noMessage();
 
@@ -108,7 +110,7 @@ int CombinedStimulus::main( void )
   string VUnit = trace( SpikeTrace[0]).unit();
 
   P.lock();
-  if (noiseduration < 60) {
+  if (noiseduration < maxDurationForIVPlot) {
     P.resize(2, 2, true);
     P[0].setXLabel("Time [ms]");
     P[0].setYLabel(trace(CurrentTrace[0]).ident() + " [" + IUnit + "]");
@@ -155,9 +157,8 @@ int CombinedStimulus::main( void )
 
     // plot
     // trace
-    P[0].plot( currenttrace, 1000.0, Plot::Yellow, 2, Plot::Solid );
     // IV
-    if (noiseduration < 60) {
+    if (noiseduration < maxDurationForIVPlot) {
       for (int i = 0; i < currenttrace.size(); i++) {
         double x = potentialtrace[i];
         double y = currenttrace[i];
@@ -170,12 +171,21 @@ int CombinedStimulus::main( void )
       ArrayD min_idcs = analyzeSamplingPulses( );//currenttrace );
       double dt = currenttrace.stepsize();
       int halfidx = min_idcs.size()/2;
+      P[0].plot( -currenttrace, 1000.0, Plot::Yellow, 2, Plot::Solid );
+      double absmin = 1000;
       for ( int i=0; i<halfidx; i++ ) {
         double ti = min_idcs[i+halfidx]*dt;
         double minimum = min_idcs[i];
-        P[0].plotPoint( ti*1000.0, Plot::First, minimum, Plot::First, 0, Plot::Circle, 5, Plot::Pixel, Plot::Green, Plot::Green );
-//        cerr << ti << ", " << minimum << "\n";
+        P[0].plotPoint( ti*1000.0, Plot::First, -minimum, Plot::First, 0, Plot::Circle, 5, Plot::Pixel, Plot::Green, Plot::Green );
+        if (minimum<absmin){
+          absmin = minimum;
+        }
+        //        cerr << ti << ", " << minimum << "\n";
+      P[0].setYRange(-.05, -absmin*1.05);
       }
+    }
+    else {
+      P[0].plot( currenttrace, 1000.0, Plot::Yellow, 2, Plot::Solid );
     }
 
     P.draw();
